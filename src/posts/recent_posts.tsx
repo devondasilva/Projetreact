35630267ca8a82aa94e3e4191b49ee15d66dfdc1
Alechou Ezechiel <alechoue@gmail.com>
2024-12-03 08:38:21 +0100

    mon_code

---
+++ src/posts/recent_posts.tsx
@@ -0,0 +1,27 @@
+import {Post,Item} from "./post"
+import i2 from "../../assets/assets/i2.jpg"
+import i3 from "../../assets/assets/i3.jpg"
+import i4 from "../../assets/assets/i4.jpg"
+import i5 from "../../assets/assets/i5.jpg"
+import i6 from "../../assets/assets/i6.jpg"
+
+export default function Recent_posts() {
+  const tab: Post[] = [
+    {image: i2, title: "Consectetuer vehicula ab", descibe: "In Kitchen, Lifestyle",},
+    {image: i3, title: "Taciti hendrerit dis odit incidunt", descibe: "In Kitchen, Living Room, Reading Room",},
+    {image: i4, title: "Sunt doloremque blandit inven", descibe: "In Living Room",},
+    {image: i5, title: "Fugit quaerat vulputate! Irure.", descibe: "In Living Room",},
+    {image: i6, title: "Litora aptent magnam laoreet!", descibe: "In Living Room",},
+  ];
+
+  return (
+      <div className="border-4 rounded mx-auto mb-10 max-w-xl py-5 px-2">
+        <p className="ml-3 font-bold text-2xl">Recent Posts</p>
+          {tab.map((post, i) => (
+            <div className={i !== tab.length-1? "border-b " : ""}>
+              <Item key={i} image={post.image} title={post.title} descibe={post.descibe}/>
+            </div>
+          ))}
+      </div>
+  );
+}
---
+++ src/tags/tagCompo.tsx
@@ -0,0 +1,14 @@
+export interface iTag {
+    name: string;
+  }
+
+  export function Titem(pop: iTag) {
+    return (
+        <div className="relative m-1 p-1">
+            <div className="relative px-4 py-2 text-sm border
+             text-red-400 hover:bg-red-500 hover:text-white">
+                  {pop.name}
+              </div>
+        </div>
+    );
+  }
